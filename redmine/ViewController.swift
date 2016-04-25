d52a60a13de0e40b6563c590fa4c52633d56d9f5
Geiser Menoia <geiser@onerhino.com>
2016-04-25 15:58:39 -0300

    Initial Commit

---
+++ redmine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  redmine
+//
+//  Created by Geiser on 25/04/16.
+//  Copyright © 2016 Geiser. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
